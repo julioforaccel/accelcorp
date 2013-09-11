9d3ee1e82094f3d522bef7f74d07584b659676ae
Julio Colomer <julio.colomer@accellion.com>
2013-09-11 15:09:15 -0400

    changed WP term on German site bottom page block

--- sites/all/themes/accellion/templates/social-news-bars1.tpl.php
+++ sites/all/themes/accellion/templates/social-news-bars1.tpl.php
@@ -415,7 +415,7 @@ print $view->preview('block');
 ?></span>
     </div>
     <div class="grid-child grid-30">
-        <span class="grid-30-t"><b>Weißbücher der Branche</b></span>
+        <span class="grid-30-t"><b>Whitepapers</b></span>
         <br><span style="word-wrap: break-word;"><?php
 $view = views_get_view('tab_views_whitepaper');
 print $view->preview('default');
@@ -450,7 +450,7 @@ print $view->preview('block');
 ?></span>
     </div>
     <div class="grid-child grid-30">
-        <span class="grid-30-t"><b>Weißbücher der Branche</b></span>
+        <span class="grid-30-t"><b>Whitepapers</b></span>
         <br><span style="word-wrap: break-word;"><?php
 $view = views_get_view('tab_views_whitepaper');
 print $view->preview('default');
