fe81744035e10a41b389a2720f7541ac709ef00d
Julio Colomer <julio.colomer@accellion.com>
2013-11-13 10:01:56 -0800

    Revert "updated Quiz module"
    This reverts commit 50420f40b37b7088d8a3cca7b5fed1dee6e0f6e1.

--- sites/all/modules/contrib/quiz/theme/quiz-report-form.tpl.php
+++ sites/all/modules/contrib/quiz/theme/quiz-report-form.tpl.php
@@ -27,23 +27,16 @@ $q_image = $p. 'question_bg.png';
 foreach ($form as $key => $sub_form):
   if (!is_numeric($key) || isset($sub_form['#no_report'])) continue;
   unset($form[$key]);
-  $c_class = ($sub_form['#is_evaluated']) ? ($sub_form['#is_correct']) ? 'q-correct' : 'q-wrong' : 'q-waiting'; ?>
+  $c_class = ($sub_form['#is_evaluated']) ? ($sub_form['#is_correct']) ? 'q-correct' : 'q-wrong' : 'q-waiting';
+  $skipped = $sub_form['#is_skipped'] ? '<span class="quiz-report-skipped">'. t('(skipped)') .'</span>' : ''?>
+
 	<div class="dt">
 	  <div class="quiz-report-score-container <?php print $c_class?>">
 	  	<span>
 	      <?php print t('Score')?>
         <?php print drupal_render($sub_form['score'])?>
         <?php print t('of') .' '. $sub_form['max_score']['#value']?>
-        <?php if ($sub_form['#is_skipped']): ?>
-          <br><em><span class="quiz-report-skipped">
-          <?php print t('(skipped)') ?>
-          </span></em>
-        <?php endif; ?>
-        <?php if ($sub_form['#is_doubtful']): ?>
-          <br><em><span class="quiz-report-doubtful">
-          <?php print t('(Doubtful)') ?>
-          </span></em>
-        <?php endif; ?>
+        <?php print '<br><em>'. $skipped .'</em>'?>
       </span>
     </div>
 	  <p class="quiz-report-question"><strong><?php print t('Question')?>: </strong></p>
